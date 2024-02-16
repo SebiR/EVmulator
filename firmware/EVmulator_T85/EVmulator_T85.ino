809589846df84b0e754e52911286225449fbf1d4
Sebastian Rietig <rietigsebastian@gmail.com>
2024-02-16 22:51:36 +0100

    Re-organized firmware, removed some debugging left-overs

--- firmware/EVmulator_T85/EVmulator_T85.ino
+++ firmware/EVmulator_T85/EVmulator_T85.ino
@@ -1,11 +1,21 @@
+/*
+
+Fuses:
+L 0xC4
+H 0xD7
+E 0xFF
+WD. Osc. 128 kHz
+*/
+
 #include <avr/sleep.h>
 #include <avr/power.h>
 #include <avr/wdt.h>
 #include <avr/io.h>
+#include <util/delay.h>
 
-#define LED_GN 0
-#define LED_RD 1
-#define CHARGE 4
+#define LED_GN 0  // PB0 - Green indicator LED
+#define LED_RD 1  // PB1 - Red indicator LED
+#define CHARGE 4  // PB4 - MOSFET to activate second resistor
 
 int pwm_good_cntr = 0;
 int pwm_bad_cntr = 0;
@@ -17,6 +27,7 @@ ISR(WDT_vect) {
 
 // Enter Sleep Mode
 void enterSleep(void) {
+  //  Unfortunately, power down stops the timers, so we can't use it
   //set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   set_sleep_mode(SLEEP_MODE_IDLE);
   sleep_enable();
@@ -30,6 +41,7 @@ void enterSleep(void) {
 
 // Setup the Watch Dog Timer (WDT)
 // http://modelleisenbahn-steuern.de/controller/atmega8/6-5-wdtcr-register-atmega8.htm
+// https://www.best-microcontroller-projects.com/attiny-ultra-low-power.html
 void setupWDT() {
 
   MCUSR &= ~(1 << WDRF);  // Clear the WDRF (Reset Flag).
@@ -38,24 +50,22 @@ void setupWDT() {
   // change WDE or the watchdog pre-scalers.
   WDTCR |= (1 << WDCE) | (1 << WDE);
 
-  // WD each 500ms
-  // WDTCR  = (0<<WDP3) | (1<<WDP2) | (0<<WDP0) | (1<<WDP0);
-
-  // WD each ~34ms
-  WDTCR = (0 << WDP2) | (1 << WDP1) | (0 << WDP0);
+  // WD triggers every ~36...37ms
+  WDTCR = (0 << WDP3) | (0 << WDP2) | (0 << WDP1) | (1 << WDP0);
 
   WDTCR |= _BV(WDIE);  // Enable the WDT interrupt.
 }
 
+//  Timer 0 is used as a counter to check the presence of the pilot PWM signal
 void setupT0() {
 
-  TCCR0A = 0x00;  //Normal mode
+  // Normal port operation, OC0A/OC0B disconnected, Normal Timer operation
+  TCCR0A = (0 << COM0A1) | (0 << COM0A0) | (0 << COM0B1) | (0 << COM0B0) | (0 << WGM01) | (0 << WGM02);
 
   // Ext. T0 as input. Rising edge
   TCCR0B = 0;
-  TCCR0B = (1 << CS02) | (1 << CS01) | (1 << CS00);
+  TCCR0B = (0 << FOC0A) | (0 << FOC0B) | (0 << WGM02) | (1 << CS02) | (1 << CS01) | (1 << CS00);
 }
-
 
 
 void setup() {
@@ -68,6 +78,8 @@ void setup() {
   pinMode(3, INPUT_PULLUP);
 
   digitalWrite(CHARGE, LOW);
+  digitalWrite(LED_GN, LOW);
+  digitalWrite(LED_RD, LOW);
 
   ADCSRA &= ~(1 << ADEN);  // Disable ADC
 
@@ -76,10 +88,16 @@ void setup() {
 }
 
 void loop() {
+  /*if ((TCNT0 >= 25) && (TCNT0 <= 45)) {
+    digitalWrite(LED_RD, 1);
+  } else {
+    digitalWrite(LED_RD, 0);
+  }*/
+
 
   // Each 36ms -> 1kHz means 36 counts
   // 30 = 900Hz, 40 =1.2kHz
-  if ((TCNT0 >= 60) && (TCNT0 <= 80)) {
+  if ((TCNT0 >= 25) && (TCNT0 <= 45)) {
     pwm_bad_cntr = 0;
     pwm_good_cntr++;
   } else {
@@ -88,28 +106,32 @@ void loop() {
   }
   TCNT0 = 0;
 
-  //approx 5 seconds of good pwm and low flag
-  if (pwm_good_cntr > 5*30 && pwm_status == 0) {
+
+  //  approx 5 seconds of good pwm and low flag
+  //  36ms period time means ~30Hz
+  if (pwm_good_cntr > 5 * 30 && pwm_status == 0) {
     pwm_status = 1;
     pwm_good_cntr = 0;
-  } else if (pwm_bad_cntr > 5*30 && pwm_status == 1) {
+  } else if (pwm_bad_cntr > 5 * 30 && pwm_status == 1) {
     pwm_status = 0;
     pwm_bad_cntr = 0;
   }
 
+  //  Pilot PWM signal is good, switch on charging indication resistor
   if (pwm_status == 1) {
-    digitalWrite(LED_GN, 1);
-    delay(1);
-    digitalWrite(LED_GN, 0);
+    digitalWrite(LED_GN, HIGH);
+    _delay_ms(2);
+    digitalWrite(LED_GN, LOW);
 
-    //digitalWrite(CHARGE, HIGH);
+    digitalWrite(CHARGE, HIGH);
 
+    // Pilot PWM signal lost, switch off charging resistor
   } else {
-    digitalWrite(LED_RD, 1);
-    delay(1);
-    digitalWrite(LED_RD, 0);
+    digitalWrite(LED_RD, HIGH);
+    _delay_ms(2);
+    digitalWrite(LED_RD, LOW);
 
-    //digitalWrite(CHARGE, LOW);
+    digitalWrite(CHARGE, LOW);
   }
 
   enterSleep();
